d0437702ddd56a38b59ed2a773423a8f6f0e9b14
zcolbert <zcolbert1993@gmail.com>
2023-04-27 17:48:37 -0700

    Edit formatting for consistency

--- sensor.c
+++ sensor.c
@@ -13,26 +13,23 @@ void read_sensor(SensorArgs* args)
     }
 }
 
-void read_counter(CounterArgs* args){
-	int count=0;
+void read_counter(CounterArgs* args)
+{
+	int count = 0;
 	double rpms = 0.0;
 	
 	while(!*(args->p_terminate))
-	{	count = 0;
+	{	
+        count = 0;
 		rpms = 0;
 		clearLS7336RCounter(args->chip_enable);
+
 		usleep(10000);
-		count  =  readLS7336RCounter(args->chip_enable);
-		rpms = (100*count)/(4*PULSES_PER_REV);
-		
-		*(args->speed_val)=rpms*(2* PI * WHEEL_RADIUS);
-		//clearLS7336RCounter(args->chip_enable);
-                //usleep(100);
-		//*(args->speed_val)=(readLS7336RCounter(args->chip_enable));
-		
 
-
+		count = readLS7336RCounter(args->chip_enable);
+		rpms = (100 * count) / (4 * PULSES_PER_REV);
+		
+		*(args->speed_val) = rpms * (2 * PI * WHEEL_RADIUS);
 	}
 }
-
 
