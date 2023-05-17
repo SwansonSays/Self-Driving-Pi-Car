2f0acd59c2fc8cbd84dbb7503ae71713df588f58
Zachary Colbert <zcolbert@sfsu.edu>
2023-05-17 14:48:28 -0700

    Add helper function to increment confidence

--- movement.c
+++ movement.c
@@ -3,6 +3,17 @@
 
 
 /**
+ * Helper function to increment a confidence value
+ * without exceeding the maximum.
+ */
+void increment_confidence(uint8_t* confidence)
+{
+    if (*confidence < CONFIDENCE_MAX) {
+        ++(*confidence);
+    }
+}
+
+/**
  * Turn left by increasing the speed of the right motor, and 
  * decreasing the speed of the left. Turning will not be performed
  * unless the sensor reading confidence threshold has been met. 
@@ -16,9 +27,7 @@ void turn_left(ProgramState* state, uint8_t* confidence)
     //printf("Turning LEFT\n");
     if (state->last_req == LEFT)
     {
-	    if (*confidence < CONFIDENCE_MAX) {
-		++(*confidence);
-	    }
+        increment_confidence(confidence);
         if (*confidence >= CONFIDENCE_THRESHOLD)
         {
             state->speed_left = Motor_Decrease_Speed(MOTOR_LEFT, state->speed_left, state->speed_left - 5, 1);
@@ -46,9 +55,7 @@ void turn_right(ProgramState* state, uint8_t* confidence)
     //printf("Turning RIGHT\n");
     if (state->last_req == RIGHT) 
     {
-	    if (*confidence < CONFIDENCE_MAX) {
-		++(*confidence);
-	    }
+        increment_confidence(confidence);
         if (*confidence >= CONFIDENCE_THRESHOLD)
         {
             state->speed_left = Motor_Increase_Speed(MOTOR_LEFT, state->speed_left, state->speed_left + 5, 1);
@@ -78,9 +85,9 @@ void go_straight(ProgramState* state, uint8_t* confidence)
     //printf("Turning RIGHT\n");
     if (state->last_req == STRAIGHT) 
     {
-	    if (*confidence < CONFIDENCE_MAX) {
-		++(*confidence);
-	    }
+        if (*confidence < CONFIDENCE_MAX) {
+            ++(*confidence);
+        }
         if (*confidence >= CONFIDENCE_THRESHOLD)
         {
             state->speed_left = Motor_Increase_Speed(MOTOR_LEFT, state->speed_left, 100, 5);
@@ -111,57 +118,57 @@ void follow_line(uint8_t line_sensor_vals[], ProgramState* state)
      * short circuit the evaluation and cases where more than one sensor are 
      * active will get skipped over.
      */
-	/* Right and outer-right */
-	if (line_sensor_vals[3] == HIGH && line_sensor_vals[4] == LOW)
-	{
-		turn_left(state, &(state->outer_confidence));
-	}
-	/* Left and outer-left */
-	else if (line_sensor_vals[3] == LOW && line_sensor_vals[4] == HIGH)
-	{
-		turn_right(state, &(state->outer_confidence));
-	}
-	else {
-	    state->outer_confidence = 0;
+    /* Right and outer-right */
+    if (line_sensor_vals[3] == HIGH && line_sensor_vals[4] == LOW)
+    {
+        turn_left(state, &(state->outer_confidence));
+    }
+    /* Left and outer-left */
+    else if (line_sensor_vals[3] == LOW && line_sensor_vals[4] == HIGH)
+    {
+        turn_right(state, &(state->outer_confidence));
+    }
+    else {
+        state->outer_confidence = 0;
 
-	    /* (1, 1, 1) All three sensors are on */
-	    if (line_sensor_vals[0] == HIGH && line_sensor_vals[1] == HIGH && line_sensor_vals[2] == HIGH) {
-		/* Keep performing the current action */
-	    }
-	    /* (1, 1, 0) LEFT and CENTER */
-	    else if (line_sensor_vals[0] == HIGH && line_sensor_vals[1] == HIGH) {
-		turn_left(state, &(state->inner_confidence));
-	    }
-	    /* (0, 1, 1) CENTER and RIGHT */
-	    else if (line_sensor_vals[1] == HIGH && line_sensor_vals[2] == HIGH) {
-		turn_right(state, &(state->inner_confidence));
-	    }
-	    /* (1, 0, 1) LEFT and RIGHT */
-	    else if (line_sensor_vals[0] == HIGH && line_sensor_vals[2] == HIGH) {
-		/* Keep performing the current action */
-	    }
-	    /* (1, 0, 0) LEFT only */
-	    else if (line_sensor_vals[0] == HIGH) {
-		turn_left(state, &(state->inner_confidence));
-	    }
-	    /* (0, 1, 0) CENTER only */
-	    else if (line_sensor_vals[1] == HIGH) {
-		go_straight(state, &(state->inner_confidence));
-	    }
-	    /* (0, 0, 1) RIGHT only */
-	    else if (line_sensor_vals[2] == HIGH) {
-		turn_right(state, &(state->inner_confidence));
-	    }
-	    /* (0, 0, 0) no sensors active */
-	    else  {
-		/* Fall back on the last attempted direction */
-		if (state->last_dir == RIGHT) {
-		    turn_right(state, &(state->inner_confidence));
-		}
-		else if (state->last_dir == LEFT) {
-		    turn_left(state, &(state->inner_confidence));
-		}
-	    }
+        /* (1, 1, 1) All three sensors are on */
+        if (line_sensor_vals[0] == HIGH && line_sensor_vals[1] == HIGH && line_sensor_vals[2] == HIGH) {
+        /* Keep performing the current action */
+        }
+        /* (1, 1, 0) LEFT and CENTER */
+        else if (line_sensor_vals[0] == HIGH && line_sensor_vals[1] == HIGH) {
+            turn_left(state, &(state->inner_confidence));
+        }
+        /* (0, 1, 1) CENTER and RIGHT */
+        else if (line_sensor_vals[1] == HIGH && line_sensor_vals[2] == HIGH) {
+            turn_right(state, &(state->inner_confidence));
+        }
+        /* (1, 0, 1) LEFT and RIGHT */
+        else if (line_sensor_vals[0] == HIGH && line_sensor_vals[2] == HIGH) {
+        /* Keep performing the current action */
+        }
+        /* (1, 0, 0) LEFT only */
+        else if (line_sensor_vals[0] == HIGH) {
+            turn_left(state, &(state->inner_confidence));
+        }
+        /* (0, 1, 0) CENTER only */
+        else if (line_sensor_vals[1] == HIGH) {
+            go_straight(state, &(state->inner_confidence));
+        }
+        /* (0, 0, 1) RIGHT only */
+        else if (line_sensor_vals[2] == HIGH) {
+            turn_right(state, &(state->inner_confidence));
+        }
+        /* (0, 0, 0) no sensors active */
+        else  {
+            /* Fall back on the last attempted direction */
+            if (state->last_dir == RIGHT) {
+                turn_right(state, &(state->inner_confidence));
+            }
+            else if (state->last_dir == LEFT) {
+                turn_left(state, &(state->inner_confidence));
+            }
+        }
     }
 }
 
